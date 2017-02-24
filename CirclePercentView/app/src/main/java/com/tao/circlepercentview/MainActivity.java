278c4168ae65126534a7238ac9e24a5fa502fee0
贾涛 <wb-jt237903@alibaba-inc.com>
2017-02-24 16:25:16 +0800

    圆形百分比控件

---
+++ CirclePercentView/app/src/main/java/com/tao/circlepercentview/MainActivity.java
@@ -0,0 +1,24 @@
+package com.tao.circlepercentview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private CirclePercentView mCirclePercentView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mCirclePercentView = (CirclePercentView) findViewById(R.id.circlePercentView);
+        mCirclePercentView.setOnCircleClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                float percent = (float) (Math.random() * 99 + 1);
+                mCirclePercentView.setCurPercent(percent);
+            }
+        });
+    }
+}
