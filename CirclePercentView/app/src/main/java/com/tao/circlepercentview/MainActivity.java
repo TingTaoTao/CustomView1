cce2b213a69e23397b80355cbabf10ac8131561e
taojia <taotao_code@163.com>
2018-04-17 10:28:22 +0800

    可自定义布局的dialog

--- CirclePercentView/app/src/main/java/com/tao/circlepercentview/MainActivity.java
+++ CirclePercentView/app/src/main/java/com/tao/circlepercentview/MainActivity.java
@@ -4,9 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.tao.circlepercentview.view.DlsCustomDialog;
+
 public class MainActivity extends AppCompatActivity {
 
     private CirclePercentView mCirclePercentView;
+    private DlsCustomDialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +21,27 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 float percent = (float) (Math.random() * 99 + 1);
                 mCirclePercentView.setCurPercent(percent);
+            }
+        });
+
+        findViewById(R.id.bt_dialog).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DlsCustomDialog.Builder builder = new DlsCustomDialog.Builder(MainActivity.this);
+                dialog = builder.cancelTouchout(false)
+                                .view(R.layout.dialog_custom_layout)
+                                .heightDimenRes(R.dimen.dialog_height)
+                                .widthDimenRes(R.dimen.dialog_width)
+                                .style(R.style.Dialog)
+                                .cancelTouchout(true)
+                                .addViewOnclick(R.id.btn_cancel,new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View view) {
+                                        dialog.dismiss();
+                                    }
+                                })
+                                .build();
+                dialog.show();
             }
         });
     }
